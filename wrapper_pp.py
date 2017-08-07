e7f2e801668d9b0169462b3e304d6a3e5a82e88b
caseynbrock <casey.brock@vanderbilt.edu>
2017-08-07 18:22:29 -0500

    intermediate changes
    will not keep these changes since i am trying to restructure code

--- wrapper_pp.py
+++ wrapper_pp.py
@@ -1,31 +1,48 @@
 #/usr/bin/env python
 
+import os
 import sys
 import ConfigParser
 
 
-# get global settings from config file
-settings = ConfigParser.ConfigParser()
-settings.read('opal.ini')
-
-
 def main():
-    random_sleep()  # sleep for random time to prevent overload of atompaw runs
-    pp_success = create_paws()  # attempt to create PAW
-    if pp_success == True:
-        results = submit_job()  # if PP creation success, submit job to test PP
+    settings = read_input_file()
+    if _constraints_violated():
+        results = {'accu': 110, 'work': 110}
     else:
-        results = (100, 100)
+        random_sleep()  # sleep for random time to prevent overload of atompaw runs
+        pp_success = create_paws()  # attempt to create PAW
+        if pp_success:
+            results = submit_job()  # if PP creation success, submit job to test PP
+        else:
+            results = {'accu': 100, 'work': 100}
     write_results_file(results)
+
+
+def _constraints_violated():
+    # read constraints file (or is it part of input?)
+    #if any statement false
+    #    return False
+    #else
+    #    return True
+    pass
+
+
+def read_inputs_file():
+    settings = ConfigParser.ConfigParser()
+    settings.read('opal.ini')
+    return settings
+
 
 def random_sleep():
     '''sleeps a random amount of time to prevent overload of atompaw runs
-    time is chosen to spread out n runs such that approximately p are 
-    running at time
+    #time is chosen to spread out n runs such that approximately p are 
+    #running at time
     '''
+    # leave this manual for now...make it auto later
     #gen_size = get_gen_size()
     #nproc = get_num_procs()
-    #t_atompaw = 
+    t_max = 
 
 
 def create_paws():
@@ -34,13 +51,17 @@ def create_paws():
 
 
 def write_results_file(results):
-    '''clean up job and write results file. 
-    This wrapper can exit at many points, each resulting in different objectives.
-    This function can be called at any point to write reults file and exit
+    '''write results file. 
+    This wrapper_pp.py script can exit at many points, each resulting in 
+    different objectives. This function can be called at any point to write 
+    results file and exit
 
-    reads in 2-tuple for accuracy objective and work objective
+    accepts Objectives object for accuracy objective and work objective
     '''
-    #with open('results.tmp') as f:
+    with open('results.tmp', 'w') as f:
+        f.write(str(results['accu'])+' accu_obj\n')
+        f.write(str(results['work'])+' work_obj\n')
+    os.rename('results.tmp', 'results')  # necessary to prevent Dakota race condition
     clean_up()
 
 
@@ -49,6 +70,7 @@ def clean_up():
     may also delete some unnecessary files
     '''
     # compress_directory()
+    # also, delete directory if constraints violated????
     pass
 
 
