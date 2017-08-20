54ca0c34a56560aa752121da3df2e094de2234b4
caseynbrock <casey.brock@vanderbilt.edu>
2017-08-20 16:25:05 -0500

    small changes and pseudopocode

--- wrapper_pp.py
+++ wrapper_pp.py
@@ -3,26 +3,28 @@
 import os
 import sys
 import ConfigParser
-
+#import logging
 
 def main():
     settings = read_input_file()
     if _constraints_violated():
         results = {'accu': 110, 'work': 110}
+        write_results_file(results)
+        # clean up
     else:
-        results = submit_job()
-        # MOVE TO SUBMIT JOB
-        # pp_success = create_paws()  # attempt to create PAW
-        # if pp_success:
-        #     results = submit_job()  # if PP creation success, submit job to test PP
-        # else:
-        #     results = {'accu': 100, 'work': 100}
-    write_results_file(results)
+        submit_batch_job()
+    # log to som main file in this directory?
 
 
-def read_input_file():
+def read_input_file(config_file='opal.ini'):
+    """
+    reads user defined options from config file opal.ini
+    (alternative config file names can be passed in for testing purposes)
+    returns a SafeConfigParser opbject so user settings can be accessed
+    using settings.get('section', 'option')
+    """
     settings = ConfigParser.SafeConfigParser()
-    settings.read('opal.ini')
+    settings.read(config_file)
     return settings
 
 
@@ -34,21 +36,31 @@ def _constraints_violated():
     #    return True
     pass
 
-def submit_job():
+
+def run_and_monitor_job():
     """
     submits job to cluster, which should run a bash script that runs a python script
     to create the pseudopotentials then run some tests. could return either real
     objectives, or false objectives based on a number of errors
     """
+    # files that are created (not by this script) at different points during the run
+    job_started_file = 'job_started'
+    job_end_file = 'job_completed'
+    job_canceled_file = 'job_canceled_walltime'
+
+    # run bash script (system call sbatch)
+    submit_job() # run this in new thread
+
+    # somehow get results back
     return {'accu': 500, 'work': 500} # return 500s for testing
 
 
-# MOVE TO DIFFERENT SCRIPT
-def create_paws():
-    '''run atompaw for each element and attempt to create pp'''
+def submit_batch_job():
+    # some sort of system call
     pass
+        
 
-
+# MAY MOVE ALL FUNCTIONALITY BELOW THIS POINT INTO SEPARATE MODULE 
 def write_results_file(results):
     '''write results file. 
     This wrapper_pp.py script can exit at many points, each resulting in 
